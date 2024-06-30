a2e3622d05db49bffd99e6f3d4f835a4b51fcd9a
Adeoye Damilola Tunde <damilolaadeoye545@yahoo.com>
2024-06-30 10:50:38 +0200

    ADD new reusable functions.

--- core/src/main/java/com/damilola/core/ext/StringUtils.kt
+++ core/src/main/java/com/damilola/core/ext/StringUtils.kt
@@ -1,5 +1,6 @@
 package com.damilola.core.ext
 
+import java.util.regex.Pattern
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.contract
 
@@ -24,7 +25,7 @@ fun String?.isNotNullOrBlank(): Boolean {
     contract {
         returns(true) implies (this@isNotNullOrBlank != null)
     }
-    return !this.isNullOrBlank()
+    return (this != null && this.isBlankOptimized())
 }
 
 fun String?.isNotEqual(secondString: String): Boolean {
@@ -39,6 +40,20 @@ fun String?.isNotContainedIn(stringArray: Array<String>): Boolean {
     return !isItEqual
 }
 
+/**
+ * @see <a href="https://www.romainguy.dev/posts/2024/speeding-up-isblank/">Romain Guy's Article</a>
+ */
+fun CharSequence.isBlankOptimized(): Boolean {
+    for (i in 0 until length - 1) {
+        val c = this[i]
+        if (!Character.isWhitespace(c) &&
+            c != '\u00a0' && c != '\u2007' && c != '\u202f'
+        ) {
+            return false
+        }
+    }
+    return true
+}
 
 fun String?.isContainedIn(stringArray: Array<String>): Boolean {
     var isItEqual = false
@@ -46,4 +61,35 @@ fun String?.isContainedIn(stringArray: Array<String>): Boolean {
         isItEqual = this.equals(eachString, true)
     }
     return isItEqual
+}
+
+val String.youtubeLink: String?
+    get() = extractYTId(this)
+
+private fun extractYTId(ytUrl: String): String? {
+    var vId: String? = null
+
+    val pattern = "(?<=watch\\?v=|/videos/|embed\\/)[^#\\&\\?]*"
+
+    val compiledPattern = Pattern.compile(pattern)
+    val matcher = compiledPattern.matcher(ytUrl)
+    if (matcher.find()) {
+        vId = matcher.group()
+    }
+
+    return vId
+}
+
+val String.gender: String
+    get() = genderParser(this)
+
+private fun genderParser(title: String): String {
+    if (title.contentEquals("Mr", true)) {
+        return "Male"
+    } else if (title.contentEquals("Mrs", true)) {
+        return "Female"
+    } else if (title.contentEquals("Miss", true)) {
+        return "Female"
+    }
+    return "Never Mind"
 }
