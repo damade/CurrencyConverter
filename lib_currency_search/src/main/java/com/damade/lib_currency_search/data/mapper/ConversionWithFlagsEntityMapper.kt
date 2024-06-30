a198b658ca3daef0290f77cff0f853bae5865171
Adeoye Damilola Tunde <damilolaadeoye545@yahoo.com>
2024-06-30 14:26:00 +0200

    MAP Domain to local storage properly.

--- lib_currency_search/src/main/java/com/damade/lib_currency_search/data/mapper/ConversionWithFlagsEntityMapper.kt
+++ lib_currency_search/src/main/java/com/damade/lib_currency_search/data/mapper/ConversionWithFlagsEntityMapper.kt
@@ -1,14 +1,13 @@
 package com.damade.lib_currency_search.data.mapper
 
+import com.damade.lib_currency_search.data.model.ConversionEntity
 import com.damade.lib_currency_search.data.model.ConversionWithFlagsEntity
 import com.damade.lib_currency_search.domain.model.ConversionWithFlags
-import com.damilola.core.mapper.EntityMapper
 import javax.inject.Inject
 
-internal class ConversionWithFlagsEntityMapper @Inject constructor() :
-    EntityMapper<ConversionWithFlagsEntity, ConversionWithFlags> {
+internal class ConversionWithFlagsEntityMapper @Inject constructor() {
 
-    override fun mapFromEntity(entity: ConversionWithFlagsEntity): ConversionWithFlags {
+    fun mapFromEntity(entity: ConversionWithFlagsEntity): ConversionWithFlags {
         with(entity) {
             return ConversionWithFlags(
                 fromCurrency = from,
@@ -22,7 +21,25 @@ internal class ConversionWithFlagsEntityMapper @Inject constructor() :
         }
     }
 
-    override fun mapToEntity(domain: ConversionWithFlags): ConversionWithFlagsEntity {
+    fun mapFromConversionEntity(
+        entity: ConversionEntity,
+        fromCurrencyFlag: String,
+        toCurrencyFlag: String,
+    ): ConversionWithFlags {
+        with(entity) {
+            return ConversionWithFlags(
+                fromCurrency = from,
+                fromCurrencyFlag = fromCurrencyFlag,
+                toCurrency = to,
+                toCurrencyFlag = toCurrencyFlag,
+                amountOfCurrencyFrom = amount,
+                amountOfCurrencyTo = result,
+                rate = rate
+            )
+        }
+    }
+
+    fun mapToEntity(domain: ConversionWithFlags): ConversionWithFlagsEntity {
         with(domain) {
             return ConversionWithFlagsEntity(
                 from = fromCurrency,
--- lib_currency_search/src/main/java/com/damade/lib_currency_search/data/repository/CurrencyConversionRepositoryImpl.kt
+++ lib_currency_search/src/main/java/com/damade/lib_currency_search/data/repository/CurrencyConversionRepositoryImpl.kt
@@ -8,15 +8,14 @@ import com.damade.lib_currency_search.data.mapper.ConversionEntityMapper
 import com.damade.lib_currency_search.data.mapper.ConversionWithFlagsEntityMapper
 import com.damade.lib_currency_search.data.mapper.SymbolEntityMapper
 import com.damade.lib_currency_search.data.model.ConversionEntity
-import com.damade.lib_currency_search.data.model.ConversionWithFlagsEntity
 import com.damade.lib_currency_search.data.model.SymbolEntity
 import com.damade.lib_currency_search.domain.model.Conversion
 import com.damade.lib_currency_search.domain.model.ConversionWithFlags
 import com.damade.lib_currency_search.domain.model.Symbol
 import com.damade.lib_currency_search.domain.repository.CurrencyConversionRepository
 import com.damilola.core.ext.isNotNullOrEmpty
-import com.damilola.core.model.Failure
 import com.damilola.core.model.Either
+import com.damilola.core.model.Failure
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
@@ -28,7 +27,7 @@ internal class CurrencyConversionRepositoryImpl @Inject constructor(
     private val currencySymbolCache: CurrencySymbolCache,
     private val conversionEntityMapper: ConversionEntityMapper,
     private val conversionWithFlagsEntityMapper: ConversionWithFlagsEntityMapper,
-    private val symbolEntityMapper: SymbolEntityMapper
+    private val symbolEntityMapper: SymbolEntityMapper,
 ) : CurrencyConversionRepository {
 
     override fun getCurrencySymbol(): Flow<List<Symbol>> {
@@ -37,14 +36,16 @@ internal class CurrencyConversionRepositoryImpl @Inject constructor(
 
             if (symbolRemote.isSuccess) {
                 val symbolEntity: List<SymbolEntity>? = symbolRemote.getSuccessOrNull()
+
                 symbolEntity?.let {
-                    emit(symbolEntityMapper.mapFromEntityList(it))
-                    currencySymbolCache.saveCurrencySymbol(it)
+                    val symbols = symbolEntityMapper.mapFromEntityList(entities = it)
+                    emit(symbols)
+                    currencySymbolCache.saveCurrencySymbol(symbol = symbols)
                 }
             } else if (symbolRemote.isError) {
-                val cachedSymbol: List<SymbolEntity>? = currencySymbolCache.fetchCurrencySymbol()
-                if (cachedSymbol.isNotNullOrEmpty() && cachedSymbol != null) {
-                    emit(symbolEntityMapper.mapFromEntityList(cachedSymbol))
+                val cachedSymbol: List<Symbol>? = currencySymbolCache.fetchCurrencySymbol()
+                if (cachedSymbol != null && cachedSymbol.isNotNullOrEmpty()) {
+                    emit(cachedSymbol)
                 } else {
                     val failureEntity: Failure? = symbolRemote.getFailureOrNull()
                     if (failureEntity != null) {
@@ -59,7 +60,7 @@ internal class CurrencyConversionRepositoryImpl @Inject constructor(
     override fun getConvertedRate(
         from: String,
         to: String,
-        amount: Int
+        amount: Int,
     ): Flow<Conversion> {
         return flow {
             val conversionRemote: Either<Failure, ConversionEntity?> =
@@ -68,8 +69,9 @@ internal class CurrencyConversionRepositoryImpl @Inject constructor(
             if (conversionRemote.isSuccess) {
                 val conversionEntity: ConversionEntity? = conversionRemote.getSuccessOrNull()
                 conversionEntity?.let {
-                    emit(conversionEntityMapper.mapFromEntity(it))
-                    currencyConversionCache.saveCurrencyConversion(it)
+                    val conversion = conversionEntityMapper.mapFromEntity(it)
+                    currencyConversionCache.saveCurrencyConversion(conversion = conversion)
+                    emit(conversion)
                 }
             } else if (conversionRemote.isError) {
                 val failureEntity: Failure? = conversionRemote.getFailureOrNull()
@@ -82,10 +84,7 @@ internal class CurrencyConversionRepositoryImpl @Inject constructor(
 
     override fun fetchConvertedRateHistory(): Flow<List<Conversion>> {
         return flow {
-            val cachedConversion: List<ConversionEntity> =
-                currencyConversionCache.fetchCurrencyConversionHistory()
-
-            emit(conversionEntityMapper.mapFromEntityList(cachedConversion))
+            emit(currencyConversionCache.fetchCurrencyConversionHistory())
         }
     }
 
@@ -98,7 +97,7 @@ internal class CurrencyConversionRepositoryImpl @Inject constructor(
         fromCurrencyFlag: String,
         to: String,
         toCurrencyFlag: String,
-        amount: Int
+        amount: Int,
     ): Flow<Conversion> {
         return flow {
             val conversionRemote: Either<Failure, ConversionEntity?> =
@@ -107,20 +106,15 @@ internal class CurrencyConversionRepositoryImpl @Inject constructor(
             if (conversionRemote.isSuccess) {
                 val conversionEntity: ConversionEntity? = conversionRemote.getSuccessOrNull()
                 conversionEntity?.let {
-                    emit(conversionEntityMapper.mapFromEntity(it))
-                    val conversionWithFlagsEntity = ConversionWithFlagsEntity(
-                        from = conversionEntity.from,
+                    val conversion = conversionEntityMapper.mapFromEntity(it)
+                    val conversionWithFlags = conversionWithFlagsEntityMapper.mapFromConversionEntity(
+                        entity = it,
                         fromCurrencyFlag = fromCurrencyFlag,
-                        to = conversionEntity.to,
                         toCurrencyFlag = toCurrencyFlag,
-                        amount = conversionEntity.amount,
-                        result = conversionEntity.result,
-                        rate = conversionEntity.rate
                     )
 
-                    currencyConversionWithFlagsCache.saveCurrencyConversion(
-                        conversionWithFlagsEntity
-                    )
+                    currencyConversionWithFlagsCache.saveCurrencyConversion(conversionWithFlags)
+                    emit(conversion)
                 }
             } else if (conversionRemote.isError) {
                 val failureEntity: Failure? = conversionRemote.getFailureOrNull()
@@ -133,10 +127,7 @@ internal class CurrencyConversionRepositoryImpl @Inject constructor(
 
     override fun fetchConvertedRateWithFlagHistory(): Flow<List<ConversionWithFlags>> {
         return flow {
-            val cachedConversion: List<ConversionWithFlagsEntity> =
-                currencyConversionWithFlagsCache.fetchCurrencyConversionHistory()
-
-            emit(conversionWithFlagsEntityMapper.mapFromEntityList(cachedConversion))
+            emit(currencyConversionWithFlagsCache.fetchCurrencyConversionHistory())
         }
     }
 
