f67986bc0ea82746d2a2dad7ff2f286aeda04418
Adeoye Damilola Tunde <damilolaadeoye545@yahoo.com>
2024-06-30 10:37:38 +0200

    ADD clear general error message for throwable.

--- core/src/main/java/com/damilola/core/ext/ExceptionUtils.kt
+++ core/src/main/java/com/damilola/core/ext/ExceptionUtils.kt
@@ -1,4 +1,6 @@
 package com.damilola.core.ext
 
+
+private const val GENERAL_ERROR_MESSAGE = "An unknown error from an exception not handled caused an Unknown failure"
 val Throwable.errorMessage: String
-    get() = message ?: localizedMessage ?: "An unknown error occurred"
+    get() = message ?: localizedMessage ?: GENERAL_ERROR_MESSAGE
