d030ddd6bbd8057b5728d3677b072065d1b56ad9
Adeoye Damilola Tunde <damilolaadeoye545@yahoo.com>
2024-06-30 10:45:50 +0200

    REMOVE unused imports.

--- libraries/remote/src/main/java/com/damilola/remote/network_factories/NetworkFactory.kt
+++ libraries/remote/src/main/java/com/damilola/remote/network_factories/NetworkFactory.kt
@@ -1,22 +1,18 @@
 package com.damilola.remote.network_factories
 
 
-import com.damilola.remote.interceptors.HttpsInterceptor
-import com.damilola.remote.interceptors.NoInternetInterceptor
-import com.damilola.remote.interceptors.UnsuccessfulCallInterceptor
 import com.squareup.moshi.Moshi
 import okhttp3.Call
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class NetworkFactory @Inject constructor(
     private val moshi: Moshi,
-    private val client: OkHttpClient) {
+    private val client: OkHttpClient,
+) {
 
     fun createRetrofit(url: String): Retrofit {
 
@@ -29,13 +25,13 @@ class NetworkFactory @Inject constructor(
 
     @Suppress("NOTHING_TO_INLINE")
     private inline fun Retrofit.Builder.delegatingCallFactory(
-        delegate: dagger.Lazy<OkHttpClient>
+        delegate: dagger.Lazy<OkHttpClient>,
     ): Retrofit.Builder = callFactory {
         delegate.get().newCall(it)
     }
 
     private inline fun Retrofit.Builder.callFactory(
-        crossinline body: (Request) -> Call
+        crossinline body: (Request) -> Call,
     ): Retrofit.Builder = callFactory { request -> body(request) }
 
 
