933dcd0d1a0eb0a213eb83359ce46cbdc6878de8
Adeoye Damilola Tunde <damilolaadeoye545@yahoo.com>
2024-06-30 13:34:07 +0200

    MODIFY class to latest api properties.

--- lib_currency_search/src/main/java/com/damade/lib_currency_search/remote/model/response/InfoResponse.kt
+++ lib_currency_search/src/main/java/com/damade/lib_currency_search/remote/model/response/InfoResponse.kt
@@ -1,8 +1,6 @@
 package com.damade.lib_currency_search.remote.model.response
 
-import java.sql.Timestamp
 
 internal data class InfoResponse(
-    //val timestamp: Timestamp? = null,
-    val rate: Double?,
+    val quote: Double?,
 )
