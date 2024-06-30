59a29f2a7699cb8603c642e07fa04dbbdc5e2ecb
Adeoye Damilola Tunde <damilolaadeoye545@yahoo.com>
2024-06-30 12:48:25 +0200

    MODIFY network response class from remote to accommodate failure.

--- lib_currency_search/src/main/java/com/damade/lib_currency_search/remote/model/response/CurrencySymbolResponse.kt
+++ lib_currency_search/src/main/java/com/damade/lib_currency_search/remote/model/response/CurrencySymbolResponse.kt
@@ -1,11 +1,10 @@
 package com.damade.lib_currency_search.remote.model.response
 
 import com.damade.lib_currency_search.remote.model.SymbolRemoteModel
+import com.damilola.core.model.RemoteErrorModel
 
 internal data class CurrencySymbolResponse (
-    val motd: MotdResponse,
-
     val success: Boolean,
-
-    val symbols: Map<String, SymbolRemoteModel>
+    val error: RemoteErrorModel?,
+    val symbols: Map<String, SymbolRemoteModel>?
 )
