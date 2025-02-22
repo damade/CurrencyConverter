3ad849a28938681f675018f53bb0e30eb017b1a8
Adeoye Damilola Tunde <30862439+damade@users.noreply.github.com>
2025-02-22 23:47:23 +0100

    UPDATE usage of version catalog in projects.

--- buildSrc/src/main/kotlin/CurrencyConverterPlugin.kt
+++ buildSrc/src/main/kotlin/CurrencyConverterPlugin.kt
@@ -49,6 +49,26 @@ class CurrencyConverterPlugin : BaseGradlePlugin() {
         }
 }
 
+internal object Config {
+    object Version {
+        const val minSdkVersion: Int = 24
+        const val compileSdkVersion: Int = 35
+        const val targetSdkVersion: Int = 35
+        const val versionName: String = "1.0"
+        const val versionCode: Int = 1
+    }
+
+    const val isMultiDexEnabled: Boolean = true
+
+    const val isViewBindingEnabled = true
+
+    object Android {
+        const val applicationId: String = "com.damilola.currencyconverter"
+        const val testInstrumentationRunner: String =
+            "androidx.test.runner.AndroidJUnitRunner"
+    }
+}
+
 private fun TestedExtension.applyCommonProperties(isAppPlugin: Boolean): TestedExtension {
     return this.apply {
         setCompileSdkVersion(Config.Version.compileSdkVersion)
